82c3796c6d6db577ba7cc1e91485f222fdb96ec5
pupsaik <stelmakh.yuras@gmail.com>
2025-12-03 13:51:48 +0200

    Add BoardMember Model

---
+++ src/Backend/TaskTracker.Domain/Entities/BoardMember.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskTracker.Domain.Entities;
+
+[Table("BoardMembers")]
+public class BoardMember
+{
+    [Key]
+    public int Id { get; set; }
+
+    public int BoardId { get; set; }
+
+    public int UserId { get; set; }
+
+    public string Role { get; set; } = "Editor";
+
+    public DateTime JoinedAt { get; set; }
+}
